665c61ecaf668c013506ac2c4825ed6718c75405
joshua_cm <bylucygrace@gmail.com>
2025-08-04 19:37:44 +0700

    file_processor.js

--- config/file_processor.js
+++ config/file_processor.js
@@ -1,31 +1,38 @@
-/*
+/* 
  * file_processor
  */
 var path = require("path");
 module.exports.file_processor = {
-   /*************************************************************************/
-   /* enable: {bool} is this service active?                                */
-   /*************************************************************************/
    enable: false,
-
-   /*************************************************************************/
-   /* basePath: {string} the root directory for where to store files        */
-   /*           make sure this matches the directory where the files volume */
-   /*           is mapped to for the container.                             */
-   /*                                                                       */
-   /*           the final stored file path should be:                       */
-   /*           basePath/[tenant.id]/file_processor/[filename.ext]          */
-   /*************************************************************************/
    basePath: path.sep + path.join("data"),
-
-   /*************************************************************************/
-   /* uploadPath: {string} the directory (under basePath) where uploaded    */
-   /*             files are stored.                                         */
-   /*************************************************************************/
    uploadPath: "tmp",
-
-   /*************************************************************************/
-   /* maxBytes: {Number} max size of uploaded file                              */
-   /*************************************************************************/
    maxBytes: 10000000,
+   maxFiles: 10,
+   allowedTypes: [
+      "image/jpeg",
+      "image/png",
+      "image/gif",
+      "image/bmp",
+      "image/webp"
+   ],
+   
+   // 文件存储策略
+   saveAs: function(file) {
+      const uuid = require('uuid').v4();
+      const ext = path.extname(file.filename);
+      return uuid + ext;
+   },
+   
+   // 响应格式
+   responseFormat: function(uploadedFiles) {
+      return {
+         success: true,
+         data: {
+            uuids: uploadedFiles.map(file => {
+               const filename = path.basename(file.fd);
+               return filename.split('.')[0];
+            })
+         }
+      };
+   }
 };
